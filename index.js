f533b842325c147ce895d2210e9a146062bbfd1a
Chris Gwilliams <chrisg@aiven.io>
2021-07-23 19:22:59 +0200

    add card format

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const PORT = process.env.PORT || 3030;
-
+let uid = require('uid');
 let app = require('express')();
 let http = require('http').createServer(app);
 //let io = require('socket.io')().listen(80);
 var cors = require('cors')
 let io = require('socket.io')(http);
 let listsOfCardsSaved = [];
-let listsNamesSaved = [];
+//let listsNamesSaved = [];
+let listsNamesSaved = [ {id: uid.uid(), name:'Backlog', cards:[]},
+     {id: uid.uid(), name: 'Next In', cards:[]},
+     {id: uid.uid(), name: 'In Progress', cards:[]},
+     {id: uid.uid(), name: 'Blocked', cards:[]},
+     {id: uid.uid(), name: 'Ready for Review', cards:[]},
+     {id: uid.uid(), name: 'Done', cards:[]}];
 
 app.use(cors());
 
@@ -14,10 +20,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api/clearLists', (req, res) => {
+   listofNamesSaved = [];
+   res.send(listofNamesSaved);
+});
+
 io.on('connection', (socket) => {
-    // console.log('a user connected');
-    if(listsOfCardsSaved!=[])
-        socket.emit('changeBoard', {listsOfCards:listsOfCardsSaved, listsNames: listsNamesSaved})
+    console.log('a user connected');
+//    if(listsOfCardsSaved!=[])
+    socket.emit('changeBoard', {lists: listsNamesSaved})
     socket.on('disconnect', () => {
       // console.log('user disconnected');
     });
@@ -25,11 +36,10 @@ io.on('connection', (socket) => {
         console.log(payload);
         io.emit('changeBoard', payload);
         listsOfCardsSaved = payload.listsOfCards;
-        listsNamesSaved = payload.listsNames;
+        listsNamesSaved = payload.lists;
     })
   });
 
 http.listen(PORT, () => {
   console.log('listening *');
 });
-
